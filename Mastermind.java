2fbbe5cfe063e8d0e6bc9062a02452e3dc48b076
meriamnarouz <117954006+meriamnarouz@users.noreply.github.com>
2023-05-29 20:59:48 -0700

    Update Mastermind.java

--- Mastermind.java
+++ Mastermind.java
@@ -13,20 +13,30 @@ public class Mastermind
 {	
 	public static void main(String[] args) 
 	{
-				
+		//list of colors in the game		
 		String[] color = {"black", "white", "green", "red", "blue", "yellow"};
 		
+		//randomly generated colors from array
 		String n1 = color[(int) (Math.random() * (6))];
 		String n2 = color[(int) (Math.random() * (6))];
 		String n3 = color[(int) (Math.random() * (6))]; 
 		String n4 = color[(int) (Math.random() * (6))];
 		
+		//variables for the user to enter colors
 		String int1 = "";
 		String int2 = "";
 		String int3 = "";
 		String int4 = "";
+		
+		//counter for checking if the user ran out of attempts or won the game
 		int count = 0;
+		
+		//variables to check how many user guesses are correct every round
+		int perfect = 0;
+		int close = 0;
+		int no = 0;
 				
+		//total attempts
 		int attemptsLeft = 10;
 		
 		//scanner class imported
@@ -34,17 +44,19 @@ public class Mastermind
 		
 		System.out.println("You have ten attempts to guess the four colors correctly!");
 		
+		//do while loop so the user can play the game at least once 
 		do
 		{
-			int perfect = 0;
-			int close = 0;
-			int no = 0;
+			//reset variables
+			perfect = 0;
+			close = 0;
+			no = 0;
 
 			System.out.print("\n\n\nNEW ATTEMPT"
 							+ "\n" + attemptsLeft + " attempt(s) left to guess the color correctly");
 			attemptsLeft--;
 			
-			
+			//take user guesses
 			System.out.print("\nEnter your guess for the first color: ");
 			int1 = in.nextLine();
 			
@@ -57,6 +69,7 @@ public class Mastermind
 			System.out.print("\nEnter your guess for the fourth color: ");
 			int4 = in.nextLine();
 			
+			//check if all four of them were guessed correctly or else
 			if(n1.equalsIgnoreCase(int1) && n2.equalsIgnoreCase(int2) && 
 					n3.equalsIgnoreCase(int3) && n4.equalsIgnoreCase(int4))
 			{
@@ -65,7 +78,11 @@ public class Mastermind
 			}
 			else
 			{		
-				//for n1
+				//else run through and see which are in the perfect place
+				//or which are the right color in the wrong place 
+				//or which are completely incorrect
+				
+				//for the first guess
 				if(n1.equalsIgnoreCase(int1))
 				{
 					perfect++;
@@ -79,7 +96,7 @@ public class Mastermind
 					no++;
 				}
 				
-				//for n2
+				//for the second guess
 				if(n2.equalsIgnoreCase(int2))
 				{
 					perfect++;
@@ -93,7 +110,7 @@ public class Mastermind
 					no++;
 				}
 				
-				//for n3
+				//for the third guess
 				if(n3.equalsIgnoreCase(int3))
 				{
 					perfect++;
@@ -107,7 +124,7 @@ public class Mastermind
 					no++;
 				}
 				
-				//for n4
+				//for the fourth guess
 				if(n4.equalsIgnoreCase(int4))
 				{
 					perfect++;
@@ -128,6 +145,7 @@ public class Mastermind
 			count++;
 		} while((attemptsLeft != 0));
 		
+		//check if the user ran out of attempts or if the user won the game
 		if(count > 9)
 		{
 			System.out.println("\n\nOh no! You lost!");
